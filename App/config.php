ef1aaa3bcac7a1ebb0b4b3ea3acbc0ba7379a3f0
mosama88 <mosama88@hotmail.com>
2024-07-03 19:09:03 +0300

    first commit

---
+++ App/config.php
@@ -0,0 +1,6 @@
+<?php
+
+
+
+define("MAIN_PATH", dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+define("URL","http://localhost:8080/CleanBlog/");
---
+++ Views/dashboard/categories/index.php
@@ -0,0 +1,72 @@
+<?php require_once("../../../App/config.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <?php include('../inc/head.php'); ?>
+
+
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php include('../inc/navbar.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="index3.html" class="brand-link">
+                <img src="<?php echo URL . "public/dashboard/" ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">AdminLTE 3</span>
+            </a>
+
+            <!-- Sidebar -->
+            <?php include('../inc/sidebar.php'); ?>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+
+            <?php include('../inc/page-header.php'); ?>
+
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+
+
+
+
+        
+
+
+
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <?php include('../inc/footer.php'); ?>
+
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+
+    <?php include('../inc/scripts.php'); ?>
+</body>
+
+</html>
